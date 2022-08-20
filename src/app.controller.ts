b056730e3e40d48be4e55c924e6b60427694faf1
Sebazt <juan.londono29@udea.edu.co>
2022-08-20 11:33:12 -0500

    start app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+export interface Iresponse{
+  appName: string,
+  appUrl:string,
+  appPort: number,
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): Iresponse {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Iresponse } from './app.controller';
+import { AppConfigService } from './config/app/config.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly appConfig: AppConfigService) {}
+
+  getHello(): Iresponse {
+    return {
+      appName: this.appConfig.name,
+      appUrl: this.appConfig.url,
+      appPort: this.appConfig.port,
+    };
   }
 }
